92a7503a59c1bfcf397924b8eec02a5812a80b7b
Kexxox <20253ds063@gmail.com>
2026-08-13 10:33:57 -0600

    cambio header

---
+++ src/test/java/com/example/maqueta_integradora/model/dao/UserDaoTest.java
@@ -0,0 +1,145 @@
+package com.example.maqueta_integradora.model.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @BeforeEach
+    void setup() {
+        UserDao userDao = new UserDao();
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void obtenerPorCorreo() {
+    }
+
+    @Test
+    void verificarTokenYActivarUsuario() {
+    }
+
+    @Test
+    void existeCorreo() {
+    }
+
+    @Test
+    void guardarCodigoRecuperacion() {
+    }
+
+    @Test
+    void validarCodigoYObtenerUsuario() {
+    }
+
+    @Test
+    void actualizarContrasena() {
+    }
+
+    @Test
+    void actualizarTokenPorCorreo() {
+    }
+
+    @Test
+    void desactivarUsuario() {
+    }
+
+    @Test
+    void activarUsuario() {
+    }
+
+    @Test
+    void updatePerfil() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testCreate() {
+    }
+
+    @Test
+    void testGetAll() {
+    }
+
+    @Test
+    void testGetById() {
+    }
+
+    @Test
+    void testUpdate() {
+    }
+
+    @Test
+    void testDelete() {
+    }
+
+    @Test
+    void testLogin() {
+    }
+
+    @Test
+    void testObtenerPorCorreo() {
+    }
+
+    @Test
+    void testVerificarTokenYActivarUsuario() {
+    }
+
+    @Test
+    void testExisteCorreo() {
+    }
+
+    @Test
+    void testGuardarCodigoRecuperacion() {
+    }
+
+    @Test
+    void testValidarCodigoYObtenerUsuario() {
+    }
+
+    @Test
+    void testActualizarContrasena() {
+    }
+
+    @Test
+    void testActualizarTokenPorCorreo() {
+    }
+
+    @Test
+    void testDesactivarUsuario() {
+    }
+
+    @Test
+    void testActivarUsuario() {
+    }
+
+    @Test
+    void testUpdatePerfil() {
+    }
+}
